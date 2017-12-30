e89bb791b97f5f6b73893a43223d580a42609b91
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-12-30 12:42:24 -0800

    Adjust width

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -56,8 +56,8 @@ class Template extends React.Component {
     return (
       <Container
         style={{
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          maxWidth: rhythm(40),
+          padding: `${rhythm(0.1)} ${rhythm(0.2 / 4)}`,
         }}>
         {header}
         {children()}
